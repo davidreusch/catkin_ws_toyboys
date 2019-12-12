2c23cf755efdc508e86aca30b2d59ec52b737442
David Reuschenberg <davidreuschenberg@web.de>
2019-12-12 21:47:31 +0100

    Auf Pi oder 0 Grad fahren klappt, bleibt die Dämpfung zu machen

---
+++ src/ass8_pdcontrol/src/drive.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from autominy_msgs.msg import Speed
+from autominy_msgs.msg import NormalizedSteeringCommand
+from autominy_msgs.msg import SpeedCommand
+
+def talker():
+    steer_pub = rospy.Publisher('/actuators/steering_normalized', NormalizedSteeringCommand, queue_size=10)
+    speed_pub = rospy.Publisher('/actuators/speed', SpeedCommand, queue_size=10)
+    rospy.init_node('publisher', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        steer_msg = NormalizedSteeringCommand()
+        steer_msg.value = 1.0
+        steer_pub.publish(steer_msg)
+        speed_msg = SpeedCommand()
+        speed_msg.value = 0.3
+        speed_pub.publish(speed_msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
--- src/ass8_pdcontrol/src/pdcontrol.py
+++ src/ass8_pdcontrol/src/pdcontrol.py
@@ -5,6 +5,7 @@ from autominy_msgs.msg import Speed, SteeringAngle, SpeedCommand, NormalizedStee
 from nav_msgs.msg import Odometry
 import numpy
 import math
+import tf
 
 goal_theta = 0
 
@@ -17,16 +18,16 @@ class OdoMeasurer():
         self.initial_y = 0
         self.theta = 0 
         self.speed = 0
-        #ceiling_cam_sub = rospy.Subscriber('/communication/gps/7', Odometry, self.ceiling_callback)
-        odom_sub = rospy.Subscriber('/sensors/localization/filtered_map', Odometry, self.odom_callback)
+        ceiling_cam_sub = rospy.Subscriber('/communication/gps/5', Odometry, self.ceiling_callback)
+#        odom_sub = rospy.Subscriber('/sensors/localization/filtered_map', Odometry, self.odom_callback)
         steering_angle_sub = rospy.Subscriber('/sensors/steering', SteeringAngle, self.steering_callback)
         speed_sub = rospy.Subscriber('/sensors/speed', Speed, self.speed_callback)
 
     def ceiling_callback(self,data):
-        self.initial_x = data.pose.pose.position.x
-        self.initial_y = data.pose.pose.position.y
+#        self.initial_x = data.pose.pose.position.x
+#        self.initial_y = data.pose.pose.position.y
         #eher 2*math.acos...?
-        self.theta = 2 * math.acos(data.pose.pose.orientation.w)
+        self.theta = tf.transformations.euler_from_quaternion([data.pose.pose.orientation.w,data.pose.pose.orientation.x,data.pose.pose.orientation.y,data.pose.pose.orientation.z])[0]
 
     def odom_callback(self,data):
         #eher 2*math.acos...?
@@ -40,6 +41,21 @@ class OdoMeasurer():
 
 
 
+
+def ferror(wanted_angle,theta):
+    return abs(wanted_angle - abs(theta))
+
+def fsteering(wanted_angle, theta):
+
+    error = 0
+    if wanted_angle == 0:
+        error = (theta / math.pi) 
+    elif wanted_angle == math.pi:
+        if theta < 0:
+            error = (math.pi + theta) / math.pi
+        else:
+            error = (math.pi - theta) / -math.pi
+    return error
 
 
 
@@ -55,33 +71,36 @@ direction = 0
 odo = OdoMeasurer()
 rospy.sleep(1)
 
-speed_command = SpeedCommand()
-speed_command.value = 0.2
-
-
 r = rospy.Rate(100)
+Kp = 0.9
+wanted_angle = 0
+theta = odo.theta
+old_error = ferror(wanted_angle, theta)
+t = 0
 while not rospy.is_shutdown():
 
     theta = odo.theta
     print(theta)
-    if theta < math.pi:
-        direction = 1  # steer to the right
-    else:
-        direction = -1  # steer to left
+    
+    error = ferror(wanted_angle, theta)
+    steering_angle = Kp * fsteering(wanted_angle, theta)
 
-    if theta < math.pi:
-        error = theta
-    else:
-        error = 2 * math.pi - theta
-
-    steering_angle = (error / math.pi) * direction
 
     pub_steering.publish(value=steering_angle)
     speed_pub.publish(value=0.2)
     
+    if (t % 5 == 0):
+        m = - (old_error - error)
+        old_error = error
+        print(m)
+
+
+    
+    t = (t+1) % 1000
     
     r.sleep()
 
+speed_pub.publish(value=0)
 
 
 rospy.spin()
