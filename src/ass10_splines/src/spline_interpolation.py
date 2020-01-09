5e702817a4d43ad4160729b087513d5e3781d8f2
David Reuschenberg <davidreuschenberg@web.de>
2020-01-09 14:16:09 +0100

    10-3 hinzugefügt, lookahead als parameter für closest-point-func eingebbar. Problem mit visualisierung bleibt

--- src/ass10_splines/src/spline_interpolation.py
+++ src/ass10_splines/src/spline_interpolation.py
@@ -27,6 +27,7 @@ def get_spline(file):
 
 
 def get_points_from_spline(arc, xspline, yspline):
+    """get an array of (x,y) Points from a spline accessed by arc"""
     xspline = xspline(arc)
     yspline = yspline(arc)
     points = []
@@ -37,6 +38,7 @@ def get_points_from_spline(arc, xspline, yspline):
 
 
 def dist(p1, p2):
+    """calc. distance between two points, second has index on position 0"""
     x1, y1 = p1
     arc, x2, y2 = p2
     return math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
@@ -53,27 +55,43 @@ def closest_dist(targetpoint, pointarray):
     return min_point
 
 
-def closest_point(spline, targetpoint):
+def closest_point(spline, targetpoint, lookahead = 0):
+    """calculate closest point to targetpoint on spline. lookahead gives a point a certain distance further on the spline"""
     arc, xspline, yspline = spline
     arclen = len(arc)
     step = 64
     low = 0
     high = arclen
+    index = 0
     while (step > 0 and low != high):
-        arcsample = arc[low:high:step]
-        xcoords = xspline(arcsample)
-        ycoords = yspline(arcsample)
 
-        # add the original indices to the points, so that we can find the 2 adjacent points in the original arc
-        points = list((zip(range(low, high, step), xcoords, ycoords)))
+        # get points on the spline from low to high with distance step, add index to retrieve point on spline later
+        points = []
+        for i in range(low, high, step):
+            points.append((i, xspline(arc[i]), yspline(arc[i])))
+
+        # get closest point to target in the sampled points
         point = closest_dist(targetpoint, points)
+
+        # get the point left an right to closest point in the sample
         index = point[0]
         low = (index - step) % arclen
         high = (index + step) % arclen
-        print(point)
-        step //= 2
 
-    return Point(x=point[1], y=point[2], z=0.0)
+        # smaller step for next iteration
+        step //= 4
+
+    # if we have lookahead, get point on spline with further lookahead
+    if lookahead > 0:
+        lookahead = (index + lookahead) % arclen
+        point = Point(x = xspline(arc[lookahead]), y = yspline(arc[lookahead]), z = 0.0)
+    else:
+        point = Point(x = point[1], y = point[2], z = 0.0)
+
+    print(point)
+
+    return point
+
 
 
 def clickCallback(data):
@@ -100,25 +118,25 @@ spline_msg.points = pointarray
 spline_msg.header.frame_id = "map"
 spline_msg.color.r = 100
 spline_msg.color.a = 100
-spline_msg.scale.x = 2
-spline_msg.scale.y = 2
-spline_msg.scale.z = 2
+spline_msg.scale.x = 0.1
+spline_msg.scale.y = 0.1
+spline_msg.scale.z = 0.1
 
 closest_point_pub = rospy.Publisher('/mypoint', Marker, queue_size=100)
 
 while not rospy.is_shutdown():
     spline_pub.publish(spline_msg)
 
-    point = closest_point(spline, (clickedPoint.x, clickedPoint.y))
+    point = closest_point(spline, (clickedPoint.x, clickedPoint.y), lookahead=200)
     click_msg = Marker()
     click_msg.type = 2
     click_msg.pose.position = point
     click_msg.header.frame_id = "map"
     click_msg.color.b = 100
     click_msg.color.a = 100
-    click_msg.scale.x = 0.3
-    click_msg.scale.y = 0.3
-    click_msg.scale.z = 0.3
+    click_msg.scale.x = 0.1
+    click_msg.scale.y = 0.1
+    click_msg.scale.z = 0.1
 
     closest_point_pub.publish(click_msg)
 
